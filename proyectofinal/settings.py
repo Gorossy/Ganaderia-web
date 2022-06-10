6b9b99f63e9a5cb4cea089736760155e852e9f81
Gorossy <dmaldonadoa@uninorte.edu.co>
2022-06-10 10:05:12 -0500

    settings

--- proyectofinal/settings.py
+++ proyectofinal/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'proyectofinal.urls'
@@ -87,6 +88,9 @@ DATABASES = {
     }
 }
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
